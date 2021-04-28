28027b46b664f683135f2787b9ac0fe4c775d6b6
Anjali Vijayvargiya <vijayvargiyaanjali@gmail.com>
2021-04-28 13:48:39 +0530

    Add files via upload

--- SeventhWebProject/Web/views.py
+++ SeventhWebProject/Web/views.py
@@ -1,1 +1,35 @@
+from django.shortcuts import render, HttpResponse
 
+# Create your views here.
+def home(request):
+    person= {'firstname': 'Amrinder', 'lastname': 'Kaur'}
+    age = 27
+    hobbies = ['Dancing', 'Singing', 'Playing Sports']
+    context= {
+        'person': person,
+        'age': age,
+        'hobbies': hobbies,
+        }
+    return render(request, 'index.html', context)
+
+def about(request):
+    person= {'firstname': 'Sukhvinder', 'lastname': 'Kaur'}
+    age = 32
+    hobbies = ['Coding', 'Programming', 'Editing']
+    context= {
+        'person': person,
+        'age': age,
+        'hobbies': hobbies,
+        }
+    return render(request, 'about.html', context)
+
+def contact(request):
+    person= {'firstname': 'Parminder', 'lastname': 'Kaur'}
+    age = 19
+    hobbies = ['Watching Movies', 'Listening Songs', 'Playing Guitar']
+    context= {
+        'person': person,
+        'age': age,
+        'hobbies': hobbies,
+        }
+    return render(request, 'contact.html', context)
