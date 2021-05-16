220f2a8f8827a95ebd9b466b2bab703ce910dd7f
Anjali Vijayvargiya <vijayvargiyaanjali@gmail.com>
2021-05-17 00:57:56 +0530

    Add files via upload

--- FifthWebProject/Web1/views.py
+++ FifthWebProject/Web1/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render, HttpResponse
 
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    #return HttpResponse("This is About Page")
+    return render(request,'about.html')
+
+def contact(request):
+    #return HttpResponse("This is Contact Page")
+    return render(request,'contact.html')
